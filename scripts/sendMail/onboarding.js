88e94ee06f77cceb43637dd10c2200b237583c81
The Mr Dec <85690985+TheMrDec@users.noreply.github.com>
2025-05-23 15:56:06 -0500

    Update onboarding.js

--- scripts/sendMail/onboarding.js
+++ scripts/sendMail/onboarding.js
@@ -1,22 +1,21 @@
 // This script requires that you activate the Admin SDK service.
+// This script also assumes that you have the onboarding document in your drive already. It will fail to fire if it cannot locate the document.
 // I have designed this script to work with a daily trigger. 
 // If you use anything other than a daily trigger, you will need to modify the onboardingWindowStartTime to reflect that change.
 
 // Set this equal to your Workspace root domain. For Example: 'example.org'
 const emailDomain = ''
-// Set this equal to your Staff OU. For Example: '/Users/Staff'
-const onboardingOrgUnit = ''
-// This sets a start time of minus 24 hours. Since the script runs every 24 hours, it shouldn't double onboard users.
-const onboardingWindowStartTime = (new Date(Date.now()).getTime() - (24*60*60*1000))
+// Add your root staff OU and your 2SV recovery OU here. We dont want to send these to students. For Example: '/Users/Staff'
+const reminderOrgUnits = ["OU1", "OU2", "OU3"]
 // You must have the onboardingEmail document in the drive of the account that you are using to run this script and that account needs to be the root owner.
 const documentName = 'onboardingEmail.pdf'
 
 // These are used for the contents of the email. See the GMailApp API usage for more options.
-const subject = "Welcome to Your New Account!"
-const body = "We are so glad to have you join our team! \n\rThere is something we need you to take care of real quick for us. For the safety and security of the organization, your account will only have limited access to domain resources, and after a while completely locked out of your account if you don't set up 2 Step Verification. \n\rPlease follow the steps in the attached document to get this set up and gain access to all of the available features of the Google Workspace domain!\n\rIf you happened to set up 2 Step Verification from a pop up that displayed when you first logged in, you can safely ignore this email. \r\nYour account will become unrestricted the day after you have set up 2SV. \r\n\r\nIf for any reason, you still do not have access to certain features after that time should have passed, please let us know by submitting a helpdesk ticket."
+const subject = "2 Step Verification Reminder"
+const body = "This is an automated email reminding you to take a few minutes to set up 2 Step Verification on your account. For the safety and security of the organization, your account will only have limited access to domain resources, and after a while completely locked out of your account if you don't set up 2 Step Verification. \n\rPlease follow the steps in the attached document to get this set up and gain access to all of the available features of the Google Workspace domain.\n\rYour account will become unrestricted the day after you have set up 2SV. \r\n\r\nIf for any reason, you still do not have access to certain features after that time should have passed, please let us know by submitting a helpdesk ticket."
 const attachment = (DriveApp.getFilesByName(documentName)).next().getAs("application/pdf")
 
-function sendOnboardingMail(){
+function send2SVReminderEmail() {
   // API query limit is 500 users so we need to do multiple queries to get all users. 
   // TODO: Maybe only get users in selected OUs to reduce API call overhead for large sites
   var pageToken
@@ -32,19 +31,22 @@ function sendOnboardingMail(){
     pageToken = gsuiteUserPage.nextPageToken
   } while (pageToken);
 
-  for (const user in  gsuiteUsers) {
+  // Check if user exists in the OUs selected above and if they are enrolled in 2 Step Verification, send reminder if needed.
+  for (const user in gsuiteUsers) {
     const currentUser = gsuiteUsers[user]
-    if ( currentUser.orgUnitPath.includes(onboardingOrgUnit) && new Date(currentUser.creationTime).getTime() > onboardingWindowStartTime) {
-      console.log(`Sending onboarding email to ${currentUser.name.fullName}`)
-      GmailApp.sendEmail(
-        currentUser.primaryEmail,
-        subject,
-        body,
-        {
-          attachments: [attachment],
-          noReply: true
-        }
-      )
+    for (const selectedOrgUnit in reminderOrgUnits) {
+      if (currentUser.orgUnitPath.includes(reminderOrgUnits[selectedOrgUnit]) && currentUser.isEnrolledIn2Sv == false) {
+        console.log(`Sending onboarding email to ${currentUser.name.fullName}`)
+        GmailApp.sendEmail(
+          currentUser.primaryEmail,
+          subject,
+          body,
+          {
+            attachments: [attachment],
+            noReply: true
+          }
+        )
+      }
     }
   }
 }
